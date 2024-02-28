aa66ba6278cc7c7ea960859a312398ebee6f63dc
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-28 11:09:39 -0300

    programa principal concluido

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,43 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Estudante;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		System.out.println("Digite o nome do Estudante: ");
+		String nome = sc.nextLine();
+
+		System.out.println("Digite a primeira nota: ");
+		double primeiraNota = sc.nextDouble();
+
+		System.out.println("Digite a segunda nota: ");
+		double segundaNota = sc.nextDouble();
+
+		System.out.println("Digite a terceira nota: ");
+		double terceiraNota = sc.nextDouble();
+
+		Estudante estudante = new Estudante(nome, primeiraNota, segundaNota, terceiraNota);
+
+		double media = estudante.media();
+
+		if (media < 60.0) {
+			System.out.println("Nota final de " + estudante.media());
+			System.out.println("Nao foi aprovador");
+			System.out.println("Falta de " + String.format("%.2f", estudante.faltandoPontos()) + "pontos!");
+		} else {
+			System.out.println("Nota final de " + estudante.media());
+			System.out.println("Estudante Aprovado !");
+		}
+
+		sc.close();
 
 	}
 
--- src/entities/Estudante.java
+++ src/entities/Estudante.java
@@ -2,4 +2,60 @@ package entities;
 
 public class Estudante {
 
+	private String name;
+	private Double primeiraNota;
+	private Double segundaNota;
+	private Double terceiraNota;
+
+	public Estudante(String name, Double primeiraNota, Double segundaNota, Double terceiraNota) {
+		this.name = name;
+		this.primeiraNota = primeiraNota;
+		this.segundaNota = segundaNota;
+		this.terceiraNota = terceiraNota;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrimeiraNota() {
+		return primeiraNota;
+	}
+
+	public void setPrimeiraNota(Double primeiraNota) {
+		this.primeiraNota = primeiraNota;
+	}
+
+	public Double getSegundaNota() {
+		return segundaNota;
+	}
+
+	public void setSegundaNota(Double segundaNota) {
+		this.segundaNota = segundaNota;
+	}
+
+	public Double getTerceiraNota() {
+		return terceiraNota;
+	}
+
+	public void setTerceiraNota(Double terceiraNota) {
+		this.terceiraNota = terceiraNota;
+	}
+
+	public Double media() {
+		return primeiraNota + segundaNota + terceiraNota;
+	}
+
+	public Double faltandoPontos() {
+		if (media() < 60.0) {
+			return 60.0 - media();
+		} else {
+			return 0.0;
+		}
+	}
+
 }
